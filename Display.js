465d0d7ac7a6a9aaec3392f21b26b6f1ef664f61
Alvaro Olarte <alvaroolartes@hotmail.com>
2023-10-17 20:26:25 -0500

    Calculadora que funcione con el teclado. aolartes

--- Display.js
+++ Display.js
@@ -8,7 +8,7 @@ class Display {
         this.valorAnterior = '';
         this.signos = {
             sumar: '+',
-            dividir: '%',
+            dividir: '/',
             multiplicar: 'x',
             restar: '-', 
         }
--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+document.addEventListener('keydown', function(event) {
+    const teclaInput = event.key;
+    if (esTeclaPermitidaNumero(teclaInput)) {
+        display.agregarNumero(teclaInput);
+    }else if(esTeclaPermitidaOperador(teclaInput)){        
+        const signos = {
+            '*': 'multiplicar',
+            '+': 'sumar',
+            '-': 'restar',
+            '/': 'dividir',
+            'Enter': 'igual'
+        }       
+        display.computar(signos[teclaInput]);
+    }else if(teclaInput === 'Backspace'){
+        display.borrar();
+    }else if(teclaInput === 'Escape'){
+        display.borrarTodo();
+    }   
+});
+
+function esTeclaPermitidaNumero(tecla) {
+    const teclasPermitidasNumeros = Array.from(botonesNumeros).map(boton => boton.textContent);
+    return teclasPermitidasNumeros.includes(tecla);
+
+    
+}
+
+function esTeclaPermitidaOperador(tecla) {    
+    const teclasPermitidasOperadores = Array.from(botonesOperadores).map(boton => boton.textContent);    
+    return teclasPermitidasOperadores.includes(tecla);
+}
+
 const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
