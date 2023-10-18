ff605443b0fce383f89b87021d5926afdfa4bdee
Alvaro Olarte <alvaroolartes@hotmail.com>
2023-10-17 21:13:36 -0500

    valorActual se pasa a string para que pueda entrar en la condición

--- Display.js
+++ Display.js
@@ -29,7 +29,7 @@ class Display {
     computar(tipo) {
         this.tipoOperacion !== 'igual' && this.calcular();
         this.tipoOperacion = tipo;
-        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorAnterior = this.valorActual.toString() || this.valorAnterior;
         this.valorActual = '';
         this.imprimirValores();
     }
